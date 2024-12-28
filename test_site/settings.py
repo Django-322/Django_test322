0c8c44b657f1a18091e34035bc77c3c27053102a
Django-322 <72962188+Django-322@users.noreply.github.com>
2024-12-28 23:07:35 +0600

    Update settings.py

--- test_site/settings.py
+++ test_site/settings.py
@@ -127,6 +127,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",  # Путь к вашей папке 'static'
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
